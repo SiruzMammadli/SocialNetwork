62255ac437f58ca4aa758cc351f78e1b27d950f2
SiruzMammadli <siruz.mammadli@gmail.com>
2022-11-14 12:30:46 +0400

    jwt token generator is added

---
+++ SocialNetwork.Core/Security/Jwt/TokenGenerator.cs
@@ -0,0 +1,36 @@
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using Microsoft.IdentityModel.Tokens;
+using SocialNetwork.Core.Entities;
+
+namespace SocialNetwork.Core.Security.Jwt
+{
+    public static class TokenGenerator
+    {
+        public static string Token(User user, string role)
+        {
+            var jwtHandler = new JwtSecurityTokenHandler();
+            var key = Encoding.UTF8.GetBytes("nmDLKAna9f9WEKPPH7z3tgwnQ433FAtrdP5c9AmDnmuJp9rzwTPwJ9yUu");
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(new[]
+                 {
+                     new Claim(JwtRegisteredClaimNames.NameId, user.Id.ToString()),
+                     new Claim(JwtRegisteredClaimNames.Email, user.Email),
+                     new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
+                     new Claim(ClaimTypes.Role, role),
+                 }),
+                Expires = DateTime.UtcNow.AddMinutes(1),
+                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key),
+                SecurityAlgorithms.HmacSha256Signature),
+                Issuer = "ComparAcademy",
+                Audience = "ComparAcademy"
+            };
+
+            var token = jwtHandler.CreateToken(tokenDescriptor);
+            return jwtHandler.WriteToken(token);
+
+        }
+    }
+}
