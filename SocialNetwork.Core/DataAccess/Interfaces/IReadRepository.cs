73a429074550e93b4a6a7c2a4ded3e2f870b8a57
SiruzMammadli <siruz.mammadli@gmail.com>
2022-11-14 12:37:37 +0400

    corePackages merged to master

--- SocialNetwork.Core/DataAccess/Interfaces/IReadRepository.cs
+++ SocialNetwork.Core/DataAccess/Interfaces/IReadRepository.cs
@@ -1,9 +1,10 @@
+using System.Linq.Expressions;
 using SocialNetwork.Core.Entities.Common;
 
 namespace SocialNetwork.Core.DataAccess.Interfaces
 {
     public interface IReadRepository<T> : IRepositoryBase<T> where T : BaseEntity, IEntity
     {
-        
+        // IEnumerable<T> GetAll(Expression<Func<T,bool>> method, bool tracking = true);
     }
 }
